3610e61c50b78d322a051fb57f2fc49147758914
level237 <Bramslevel129@gmail.com>
2024-01-07 12:32:18 +0100

    list food data

--- src/components/Food/Food.jsx
+++ src/components/Food/Food.jsx
@@ -1,10 +1,11 @@
 import food1 from "../../img/food1.jpeg"
 
+
 const Food=()=>{
 
     return (
         <>
-            <div className="grid grid-cols-3 gap-5">
+            <div className="grid grid-cols-4 gap-5">
                 <div class="w-full pb-6 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
                         <img src={food1} class="p-8 rounded-t-lg"  alt="product image" />
---
+++ src/data/foods.js
@@ -0,0 +1,61 @@
+import food2 from "../img/food2.jpeg";
+import food3 from "../img/food3.jpeg";
+import food4 from "../img/food4.jpeg";
+import food5 from "../img/food5.jpeg";
+import food6 from "../img/food6.jpeg";
+import food7 from "../img/food7.jpeg";
+import food8 from "../img/food8.jpeg";
+import food9 from "../img/food9.jpeg";
+
+const foods=[{
+    id:1,
+    name:"Ikok Sucré",
+    category:"African",
+    price:"12",
+    img:food2
+},
+{
+    id:2,
+    name:"Taro Sauce Jaune",
+    category:"African",
+    price:"12",
+    img:food3
+},{
+    id:3,
+    name:"Spaghetti Sauté",
+    category:"African",
+    price:"12",
+    img:food4
+},{
+    id:4,
+    name:"Atieké",
+    category:"African",
+    price:"12",
+    img:food5
+},{
+    id:5,
+    name:"Garba",
+    category:"African",
+    price:"12",
+    img:food6
+},{
+    id:6,
+    name:"Alloco",
+    category:"African",
+    price:"12",
+    img:food7
+},{
+    id:7,
+    name:"Placali",
+    category:"African",
+    price:"12",
+    img:food8
+},{
+    id:8,
+    name:"Le foutu banane",
+    category:"African",
+    price:"12",
+    img:food9
+}]
+
+export default foods;
